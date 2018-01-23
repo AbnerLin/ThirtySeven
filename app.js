067fb08314cc1fb95c4e61b3f17ff4168a324605
AbnerLin <shaoyang0805@gmail.com>
2018-01-23 14:57:31 +0800

    Adjust architecture, and services code are not modify yet, so this version will build error.

--- app.js
+++ app.js
@@ -1,20 +1,15 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require(path.join(process.cwd(), 'lib', 'logger'));
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var session = require('express-session');
-var app = express();
+const express = require('express');
+const path = require('path');
+const logger = require(path.join(process.cwd(), 'lib', 'logger'));
+const session = require('express-session');
 
-var expressRouteLoader = require(path.join(process.cwd(), 'lib', 'express-route-loader'))(app);
+const auth = require(path.join(process.cwd(), 'routes', 'auth'));
+const customer = require(path.join(process.cwd(), 'routes', 'customer'));
+const map = require(path.join(process.cwd(), 'routes', 'map'));
+const menu = require(path.join(process.cwd(), 'routes', 'menu'));
+const order = require(path.join(process.cwd(), 'routes', 'order'));
 
-/** view engine setup */
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-/** uncomment after placing your favicon in /public */
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+const app = express();
 
 /** Session */
 app.use(session({
@@ -23,14 +18,10 @@ app.use(session({
         httpOnly: false,
         maxAge: 24 * 60 * 60 * 1000
     },
-    secret: '1234567890QWERT'
+    secret: '1234567890QWERT',
+    resave: true,
+    saveUninitialized: true
 }))
-
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 /** Logger setting. */
 app.use(function(req, res, next) {
@@ -39,28 +30,10 @@ app.use(function(req, res, next) {
 });
 
 /** Load route */
-expressRouteLoader.dir(path.join(__dirname, 'routes'));
-
-/** catch 404 and forward to error handler */
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-/** error handler */
-app.use(function(err, req, res, next) {
-    /** set locals, only providing error in development */
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    /** render the error page */
-    res.status(err.status || 500);
-
-    /** logger */
-    logger.info(logger.responsePattern(res));
-
-    res.render('error');
-});
+app.use('/api/auth', auth);
+app.use('/api/customer', customer);
+app.use('/api/map', map);
+app.use('/api/menu', menu);
+app.use('/api/order', order);
 
 module.exports = app;
